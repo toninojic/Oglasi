a21e1b3112c98cc52fd00dc7cf4237d47b487cd1
Antonije Nojic <antonijenojic01@gmail.com>
2022-07-02 15:01:33 +0200

    Done styling on register and login form

--- src/Views/login.php
+++ src/Views/login.php
@@ -1,17 +1,11 @@
-<div>
-    <h1>This is Login page</h1>
-</div>
-
-
-<div class="login">
-
+<div class="login-form">
     <form action="index.php?page=login" method="POST">
-        <p>Username:</p><br />
-        <input type="text" name="username" placeholder="Enter your username"><br />
-        <p>Password:</p><br />
-        <input type="password" name="password" placeholder="Enter your password"><br />
-        <input type="submit" value="Login">
+        <p>Username:</p>
+        <input type="text" name="username" placeholder="Enter your username">
+        <p>Password:</p>
+        <input type="password" name="password" placeholder="Enter your password">
         <div class="error"><p><?php echo $errors;?></p></div>
+        <input id="login" type="submit" value="Login">
     </form>
 
 </div>
--- src/Views/register.php
+++ src/Views/register.php
@@ -1,24 +1,18 @@
-<div>
-    <h1>This is Register page</h1>
-</div>
-
-
 <div class="register-form">
-    
     <form action="index.php?page=register" method="POST">
-        <p>Full name:</p><br />
-        <input type="text" name="fullName" placeholder="Enter your full name"/><br />
-        <p>Username:</p><br />
-        <input type="text" name="username" placeholder="Enter your username"/><br />
-        <p>Email:</p><br />
-        <input type="text" name="email" placeholder="Enter your email"/><br />
-        <p>Password:</p><br />        
-        <input type="password" name="password" placeholder="Enter password"/><br />
-        <p>Confirm password:</p><br />       
-        <input type="password" name="passwordConfirm" placeholder="Confirm password"/><br /><br /> 
-
+        <p>Full name:</p>
+        <input type="text" name="fullName" placeholder="Enter your full name"/>
+        <p>Username:</p>
+        <input type="text" name="username" placeholder="Enter your username"/>
+        <p>Email:</p>
+        <input type="text" name="email" placeholder="Enter your email"/>
+        <p>Password:</p>
+        <input type="password" name="password" placeholder="Enter password"/>
+        <p>Confirm password:</p>      
+        <input type="password" name="passwordConfirm" placeholder="Confirm password"/>
         <div class="error"><p><?php echo $errors;?></p></div>
-        <input type="submit" value="Register"/>
+        <input id="register" type="submit" value="Register"/>
+        <p>Already have an account? Please <a href="index.php?page=login">login</a> here.</p>
     </form>
 
 </div>
